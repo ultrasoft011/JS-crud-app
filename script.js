1559d64b9e40ed6e9804a30fda9950489ad95d22
ultrasoft011 <ultrasoft.011@gmail.com>
2021-07-30 19:37:13 -0500

    first crud JS app

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict";
+// const uniqid = require("uniqid");
+
+// Select the items
+
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+
+// Edit options
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// Submit form
+
+form.addEventListener("submit", addItem);
+
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  console.log(value);
+  const id = 11; // uniqid(); has to be change
+  // console.log(id);
+  if (value && !editFlag) {
+    // console.log("add item");
+    const element = document.createElement("article");
+    // adding the class to the new element -> article
+    element.classList.add("grocery-item");
+    // console.log(element);
+
+    // adding the id
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          `;
+    // console.log(element);
+
+    // Append child
+    list.appendChild(element);
+    // Display alert
+    displayAlert("Added succesfully", "success");
+    // Render the container
+    container.classList.add("show-container");
+    console.log(container);
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    displayAlert("Emptyy", "danger");
+  }
+}
+
+// Function: display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // Remove the alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 4000);
+}
